67d719115b7ec59670000d3f1a9d93250ec3b36c
Galuh Wasesa <surat.sesha@gmail.com>
2025-07-18 20:26:17 +0700

    init prisma migration

---
+++ prisma/migrations/20250718132040_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Condiment" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "spicy" TEXT NOT NULL,
+
+    CONSTRAINT "Condiment_pkey" PRIMARY KEY ("id")
+);
